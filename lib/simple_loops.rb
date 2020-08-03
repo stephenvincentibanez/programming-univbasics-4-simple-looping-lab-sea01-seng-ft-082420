cf5ed8f00123cfe62849c5ecef82448b360fb08b
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-03 20:10:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   counter=0 
+  message="Hello World"
   
   while counter <= 5
-  puts string[counter]
+  puts message[counter]
   counter+=1 
 end
 end
