875a792c13919c132685bd2df7e7f54963fcb14d
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-03 20:24:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,7 +10,11 @@ end
 
 def loop_message_n_times(message, n)
   i=0 
-  while i < 5
+  if i < 5
+  message="Hello Moon."
+  puts message
+  i+=1
+else 5 >= i < 10 
   message="Hello Red Balloon."
   puts message
   i+=1
